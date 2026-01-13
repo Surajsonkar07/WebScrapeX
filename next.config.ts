c29a90e300020b1dd657e3bb100817a355aa50a8
Surajsonkar07 <sonkarsuraj447@gmail.com>
2026-01-14 01:37:35 +0530

    ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['puppeteer', 'puppeteer-core', 'website-scraper', 'website-scraper-puppeteer'],
+  serverExternalPackages: ['puppeteer-core'],
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core'],
+    // @ts-ignore
+    outputFileTracingExcludes: {
+      '*': [
+        './tmp/**/*',
+        './node_modules/@puppeteer/browsers/**/*',
+        './node_modules/puppeteer-core/.local-chromium/**/*',
+        './node_modules/canvas/**/*',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
